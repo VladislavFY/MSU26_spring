8452b887ef2fedc9a511c7f71540d0ee03d8eed0
VladislavFY <vladislavfrolov2008@gmail.com>
2026-03-10 21:47:57 +0300

    Update Task3_func.c

--- Task3/Task3_func.c
+++ Task3/Task3_func.c
@@ -62,24 +62,4 @@ int search(const char *SInputFile, char *word) {
     return 0;
 }
 
-int main(void)
-{
-    char filename[] = "input.txt";   /* имя файла */
-    char result[513];                /* буфер для найденного слова */
-    int r;
 
-    r = search(filename, result);
-
-    if (r == -1) {
-        printf("Cannot open file\n");
-        return 1;
-    }
-
-    if (result[0] != '\0') {
-        printf("Longest word: %s\n", result);
-    } else {
-        printf("No suitable word found\n");
-    }
-
-    return 0;
-}
